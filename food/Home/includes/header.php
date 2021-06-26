cb11b82bd1799711c71cc4c15d5fab8b43860b75
Supun Lakshan <59835161+smartsupun@users.noreply.github.com>
2021-06-26 19:21:52 +0530

    Home page and item pages added

---
+++ food/Home/includes/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/jaygurubande.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <link rel="shortcut icon" href="images/logo.png">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <!-- <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> -->
+    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="/css/ricestyle.css"> -->
+    <link rel="stylesheet" href="css/number.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <!-- top nav bar -->
+    <div class="container-fluid ">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar ">
+            <div class="container">
+                <img width="80px" height="40px" src="images/logo.png" alt="logo">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse navi" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+
+                        <li class="nav-item active"><a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                About us
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">About Company</a>
+                                <a class="dropdown-item" href="#">Vision & Mission</a>
+                            </div>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="#category">Categories</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Cart</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!-- ----nav bar end------------ -->
